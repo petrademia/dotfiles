d998a0f9f0651d8b196cb45a7408fff36c737b3a
Petrus Damianus Sammy Wiyadi <petrus.dsw@gmail.com>
2026-07-07 21:24:53 +0700

    fix: add llvm/bin to PATH - brew llvm is keg-only

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -3,7 +3,8 @@ eval "$(fnm env --use-on-cd)"
 [ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 [ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && . "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# Brew rustup is keg-only
+# Brew keg-only formulas
 export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
+export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
 
 export PATH="$HOME/.local/bin:$PATH"
