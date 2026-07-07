32e4e1ae2ee496eda54d20f98559b268a409b28f
Petrus Damianus Sammy Wiyadi <petrus.dsw@gmail.com>
2026-07-07 21:20:39 +0700

    fix: add rustup/bin to PATH permanently in zsh config

--- config/zsh/path.zsh
+++ config/zsh/path.zsh
@@ -3,4 +3,7 @@ eval "$(fnm env --use-on-cd)"
 [ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 [ -s "$HOME/.sdkman/bin/sdkman-init.sh" ] && . "$HOME/.sdkman/bin/sdkman-init.sh"
 
+# Brew rustup is keg-only
+export PATH="/opt/homebrew/opt/rustup/bin:$PATH"
+
 export PATH="$HOME/.local/bin:$PATH"
